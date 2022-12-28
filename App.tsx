2cac1ad19320e8ceb1729d02b3150ad8a24b60b6
Daniel Marques <danielmarques.enfermagem@gmail.com>
2022-12-28 19:07:56 -0300

    Final Version v3

--- App.tsx
+++ App.tsx
@@ -1,7 +1,3 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Logo from "./src/components/svgComponents/Logo";
-import Clipboard from "./src/components/svgComponents/Clipboard";
 import Home from "./src/screens/Home";
 import { StatusBar } from "react-native";
 export default function App() {
